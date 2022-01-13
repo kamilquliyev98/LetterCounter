5e4f7962a4296a3b0f08d6b21a97d5e6a8d50a45
Kamil Guliyev <94452586+kamilquliyev98@users.noreply.github.com>
2022-01-13 17:58:10 +0400

    first-commit

---
+++ ABC-Counter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ABC_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Hello";
+
+            while (word.Length > 0)
+            {
+                int count = 0;
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[0] == word[i])
+                    {
+                        count++;
+                    }
+                }
+
+                Console.WriteLine($"{word[0]} = {count}");
+                word = word.Replace(word[0].ToString(), "");
+            }
+
+        }
+    }
+}
